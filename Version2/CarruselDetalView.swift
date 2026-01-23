0c24fbebc7f0cb515a51416ec14748e4f65011b2
Fabricio Banda <fabribanda438@gmail.com>
2026-01-23 10:26:39 -0600

    Final Version

---
+++ Version2/CarruselDetalView.swift
@@ -0,0 +1,18 @@
+//
+//  CarruselDetalView.swift
+//  Version2
+//
+//  Created by Fabricio Banda on 23/01/26.
+//
+
+import SwiftUI
+
+struct CarruselDetalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarruselDetalView()
+}
